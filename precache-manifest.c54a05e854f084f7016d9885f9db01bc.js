f01cbc5101267ee91671ef4965dce3bce91950fc
Josiah Chan <horming8@mbp-jiraiya.lan>
2020-09-25 11:16:13 -0700

    Updates

---
+++ precache-manifest.c54a05e854f084f7016d9885f9db01bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c00d0b379664b51b94a88f06bec607cf",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "77192a2ca987a9188501",
+    "url": "/monsters-rolodex/static/css/main.b6285e9d.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77192a2ca987a9188501",
+    "url": "/monsters-rolodex/static/js/main.37c19114.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
